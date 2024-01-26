16c8bb608eda2c3f175c197508ecc5ad9a527d38
mnik7044 <mnikwork7802@gmail.com>
2024-01-26 21:08:29 +0530

    Added ph no and email in schema

--- models/case.js
+++ models/case.js
@@ -27,6 +27,14 @@ const caseSchema = new mongoose.Schema({
     type: Number,
     required: true,
   },
+  phoneNumber: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
   address: {
     type: String,
     required: true,
