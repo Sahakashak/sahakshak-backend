fb79c67c1570f8e7a21c8645679408e85e85972d
mnik7044 <mnikwork7802@gmail.com>
2024-01-27 13:55:54 +0530

    Removed cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 require("dotenv").config();
 const caseRoutes = require("./routes/caseRoutes");
 const timeoutMiddleware = require("./Middleware/timeout");
@@ -9,6 +10,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(timeoutMiddleware);
+app.use(cors());
 
 // Routes
 app.use("/api", caseRoutes);
